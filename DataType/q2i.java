ee2fd2d7597bdda7c784e49066c944d14409c438
bibek <bibekmagar.965@gmail.com>
2021-09-30 15:17:23 +0545

    first commit

---
+++ DataType/q2i.java
@@ -0,0 +1,13 @@
+//WAP to print pattern
+
+package Java.LabQuestions.DataType;
+
+public class q2i {
+    public static void main(String[] args) {
+        int sum = 0;
+        for(int i =1 ; i<=5; i++){
+            sum= sum*10 + i;
+            System.out.println(sum);
+        }
+    }
+}
---
+++ DataType/q3.java
@@ -0,0 +1,14 @@
+//WAP to show the use of left shift and right shift operetor
+
+package Java.LabQuestions.DataType;
+
+public class q3 {
+    public static void main(String[] args) {
+        int a=32;
+		int i;
+		i=a>>2;
+		System.out.println("the value of a before right shfit is: " +a);
+		System.out.println("the value of a after applying right shfit is: " +i);
+
+    }
+}
